5cad8657669c50ba3e61da5dab33595fb69b4d87
Brittany Ohalete <bopho@Brittanys-MacBook-Pro.local>
2019-04-15 11:40:31 -0400

    Grab url value

--- image.js
+++ image.js
@@ -1,2 +1,13 @@
 /* This assignment tests your understanding of JavaScript and its interaction with HTML user interfaces and
 DOM.*/
+
+//Checks if url entered is valid
+function validateURL(){
+
+}
+
+//Grab users entered url and display the photo
+function AddPhoto(){
+  var user-url = $('image-url').value);
+  validateURL(user-url);
+}
