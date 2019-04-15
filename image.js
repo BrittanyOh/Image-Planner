ecf5a39b66482b870b79171086f13f90d025247f
Brittany Ohalete <bopho@Brittanys-MacBook-Pro.local>
2019-04-15 12:38:22 -0400

    Grab all image src values

--- image.js
+++ image.js
@@ -1,7 +1,7 @@
 /* This assignment tests your understanding of JavaScript and its interaction with HTML user interfaces and
 DOM.*/
 
-//Checks if url entered is valid
+//CHECK IF VALID URL
 function validateURL(url){
   var regex = new RegExp('^(https?:\\/\\/)?'+ // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
@@ -12,16 +12,33 @@ function validateURL(url){
   return !!regex.test(url);
 }
 
-//Grab users entered url and display the photo
+
+//GRABS USERS ENTERED URL AND DISPLAY PHOTO
 function AddPhoto(){
   var user_url = ($('image-url').value);
   var img = document.createElement("img");
 
-  //check if url is valud
+  //check if url is valid
   if(validateURL(user_url)){
+    //if valid set the image src and class then append to planning area
     img.src = user_url;
     img.className = "planning-img";
     $('planning-area').appendChild(img);
+  }
+  //else print error message, toggle display
+  else{
+    $('url-error').classList.toggle("alert");
+  }
+}
+
+//GRABS USER ENTERED URL AND REMOVES FROM PLANNING AREA
+function DeletePhoto(){
+  var remove_url = ($('image-url').value);
+  var src = $('img').getAttribute("src");
+  var remove_img;
+  //check if url is valid
+  if(validateURL(remove_url)){
+    alert(src);
   }
   //else print error message, toggle display
   else{
